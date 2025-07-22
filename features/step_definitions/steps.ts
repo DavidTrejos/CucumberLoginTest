5464d4ffa458e22253024080c27f1f709ae4e16f
Davidtrejos77-star <david.trejos@linktic.com>
2025-07-22 00:38:21 -0500

    Triple AAA Pattern to test automation

--- features/step_definitions/steps.ts
+++ features/step_definitions/steps.ts
@@ -4,14 +4,14 @@ import { chromium } from 'playwright';
 import { Page } from 'playwright';
 
 let page: Page;
-
+//Arrange
 Given('the user navigates to the login page', async () => {
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   page = await context.newPage();
   await page.goto('https://the-internet.herokuapp.com/login');
 });
-
+//Act
 When('the user enters invalid credentials', async () => {
   await page.fill('#username', 'tomsmith');
   await page.fill('#password', 'SuperSecretPassword');
@@ -25,7 +25,7 @@ When('the user enters valid credentials', async () => {
 When('clicks the login button', async () => {
   await page.click('button[type="submit"]');
 });
-
+//Assert 
 Then('the user should be stay in login', async () => {
   await page.waitForURL('**/login');
   expect(page.url()).toContain('/login');
